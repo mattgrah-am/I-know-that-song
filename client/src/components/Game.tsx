c9a88752558ae6618f3059ceb7361f5592eaf204
Matt Graham <iam@mattgrah.am>
2022-04-19 07:22:28 +1000

    add routes to game mode

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,18 +1,25 @@
+import { Card, Container } from "@mui/material";
 import { useContext } from "react";
 import { ArtistDataContext } from "../Context/artistDataContext";
 
 export const Game = () => {
   const { artistData } = useContext<any>(ArtistDataContext);
-  return artistData.map((track: any) => (
-    <div>
-      <p>{track.song}</p>
-      <img src={track.image} alt="" className="album-art" />
-      {track.preview && (
-        <audio controls>
-          <source src={track.preview} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
-      )}
-    </div>
-  ));
+  return (
+    <Container>
+      {artistData.map((track: any, index: number) => (
+        <Card variant="outlined" className="artist-search" key={index}>
+          <img src={track.image} alt="" className="album-art" />
+          <div>
+            <p>{track.song}</p>
+            {track.preview && (
+              <audio controls>
+                <source src={track.preview} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            )}
+          </div>
+        </Card>
+      ))}
+    </Container>
+  );
 };
